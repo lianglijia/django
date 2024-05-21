fb87d08508fc762058bb4f5903a33a4904f48421
lianglijia <1121468354@qq.com>
2024-05-21 18:30:39 +0800

    20240521_commit

--- learning_log/learning_logs/views.py
+++ learning_log/learning_logs/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import Topic
+from .models import Topic, Entry
+from .forms import TopicForm, EntryForm
 
 
 # Create your views here.
@@ -22,3 +24,55 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by("-date_added")
     context = {"topic": topic, "entries": entries}
     return render(request, "learning_logs/topic.html", context)
+
+
+def new_topic(request):
+    """添加新主题"""
+    if request.method != "POST":
+        # 未提交数据：创建⼀个新表单
+        form = TopicForm()
+    else:
+        # POST 提交的数据：对数据进⾏处理
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("learning_logs:topics")
+    # 显⽰空表单或指出表单数据⽆效
+    context = {"form": form}
+    return render(request, "learning_logs/new_topic.html", context)
+
+
+def new_entry(request, topic_id):
+    """在特定主题中添加新条⽬"""
+    topic = Topic.objects.get(id=topic_id)
+    if request.method != "POST":
+        # 未提交数据：创建⼀个空表单
+        form = EntryForm()
+    else:
+        # POST 提交的数据：对数据进⾏处理
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return redirect("learning_logs:topic", topic_id=topic_id)
+    # 显⽰空表单或指出表单数据⽆效
+    context = {"topic": topic, "form": form}
+    return render(request, "learning_logs/new_entry.html", context)
+
+
+def edit_entry(request, entry_id):
+    """编辑既有的条⽬"""
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+    if request.method != "POST":
+        # 初次请求：使⽤当前的条⽬填充表单
+        form = EntryForm(instance=entry)
+    else:
+        # POST 提交的数据：对数据进⾏处理
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("learning_logs:topic", topic_id=topic.id)
+    context = {"entry": entry, "topic": topic, "form": form}
+    return render(request, "learning_logs/edit_entry.html", context)
--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 a = 1
-b = 2
+b = 20
 c = a + 1
 d = c + 1
 print(a)
